94284dc43ca6723ec20ec63664e6eabcc68abcae
Michał Sypko <m.sypko@livechatinc.com>
2017-09-30 15:51:20 +0200

    v2 js files

---
+++ 0.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Golden Eye</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<div class="center">
+    <div class="inner">
+
+        <p>Press enter to login</p>
+
+    </div>
+</div>
+
+<script src="js/jquery.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by michalsypko on 30/09/2017.
+ */
